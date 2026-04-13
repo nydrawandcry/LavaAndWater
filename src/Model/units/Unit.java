c57169557d20c4f0b4f2e3bc9a5da176b3fa953a
ная <nastya034rus@yandex.ru>
2026-04-13 16:40:13 +0300

    Созданы поля для класса Unit

--- src/Model/units/Unit.java
+++ src/Model/units/Unit.java
@@ -1,4 +1,10 @@
 package Model.units;
 
+import Model.gamefield.Cell;
+
 public abstract class Unit {
+
+    private Cell _owner;
+    private boolean _isActive;
+    private boolean _isDestroyed;
 }
