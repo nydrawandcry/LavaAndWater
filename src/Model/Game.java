f674accbed603a1fad14cbd0708663124c027836
ная <nastya034rus@yandex.ru>
2026-04-14 02:52:11 +0300

    Добавлены методы для реализации механик игры в классе Game

--- src/Model/Game.java
+++ src/Model/Game.java
@@ -3,7 +3,10 @@ package Model;
 import Model.gamefield.Gamefield;
 import Model.services.CollisionDetector;
 import Model.services.Maze;
+import Model.units.Exit;
 import Model.units.Player;
+import Model.units.liquids.Lava;
+import Model.units.liquids.Water;
 
 public class Game {
 
@@ -15,6 +18,39 @@ public class Game {
     private CollisionDetector _collisionDetector = new CollisionDetector();
 
     
+
+    private void spreadLiquids() {
+        new Lava().expand(_field);
+        new Water().expand(_field);
+    }
+
+    private void updateGameState() {
+        if (!_player.isAlive()) {
+            _isOver = true;
+            _isWon = false;
+            return;
+        }
+
+        if (isPlayerOnExit()) {
+            _isOver = true;
+            _isWon = true;
+        }
+    }
+
+    private boolean isPlayerOnExit() {
+        return _player.owner().getUnit(Exit.class) != null;
+    }
+
+    private Player findPlayer() {
+        for (var cell : _field) {
+            Player p = (Player) cell.getUnit(Player.class);
+            if (p != null) {
+                return p;
+            }
+        }
+
+        throw new IllegalStateException("Игрок не найден на поле");
+    }
 
     public Gamefield getField() {
         return _field;
