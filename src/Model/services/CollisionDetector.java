844061bc70983c520ccf2728f4189bc409af39d2
ная <nastya034rus@yandex.ru>
2026-04-14 02:17:09 +0300

    Реализована обработка столкновений в классе CollisionDetector

--- src/Model/services/CollisionDetector.java
+++ src/Model/services/CollisionDetector.java
@@ -1,11 +1,23 @@
 package Model.services;
 
 import Model.gamefield.Cell;
+import Model.gamefield.Gamefield;
 import Model.units.impassable.Wall;
 import Model.units.liquids.Lava;
 import Model.units.liquids.Water;
 
 public class CollisionDetector {
+
+    public void resolve(Gamefield field) {
+        for(Cell c : field) {
+            boolean hasLava = c.getUnit(Lava.class) != null;
+            boolean hasWater = c.getUnit(Water.class) != null;
+
+            if(hasLava && hasWater) {
+                createWallsInCell(c);
+            }
+        }
+    }
 
     private void createWallsInCell(Cell cell) {
         cell.extractUnit(cell.getUnit(Lava.class));
