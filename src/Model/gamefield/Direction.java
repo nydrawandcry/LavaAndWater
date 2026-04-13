e92bd0b8451958c8092818075cb8ef778bcf386e
ная <nastya034rus@yandex.ru>
2026-04-13 16:05:45 +0300

    Добавлены стороны света для навигации в класс Direction

--- src/Model/gamefield/Direction.java
+++ src/Model/gamefield/Direction.java
@@ -1,4 +1,16 @@
 package Model.gamefield;
 
 public enum Direction {
+
+    NORTH(0),
+    SOUTH(6),
+    EAST(3),
+    WEST(9);
+
+    private final int _hours;
+
+    Direction(int hours){
+        _hours = hours % 12;
+    }
+    
 }
