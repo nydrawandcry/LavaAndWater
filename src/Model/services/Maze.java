1cd003c0f44429d61f8d52a6c4a5ca07ebd79e85
ная <nastya034rus@yandex.ru>
2026-04-14 02:06:55 +0300

    Добавлены методы расстановки юнитов в клетках и реализован метод equipCells в Maze

--- src/Model/services/Maze.java
+++ src/Model/services/Maze.java
@@ -1,6 +1,13 @@
 package Model.services;
 
+import Model.gamefield.Cell;
 import Model.gamefield.Gamefield;
+import Model.units.Exit;
+import Model.units.Player;
+import Model.units.impassable.IronBlock;
+import Model.units.impassable.Wall;
+import Model.units.liquids.Lava;
+import Model.units.liquids.Water;
 
 public class Maze {
 
@@ -12,6 +19,7 @@ public class Maze {
         }
         _config = config;
     }
+    
     public Gamefield buildField() {
         Gamefield field = new Gamefield(_config.getHeight(), _config.getWidth());
         equipCells(field);
@@ -19,6 +27,51 @@ public class Maze {
     }
 
     private void equipCells(Gamefield field) {
+        placeWalls(field);
+        placeIronBlocks(field);
+        placeLava(field);
+        placeWater(field);
+        placePlayer(field);
+        placeExit(field);
+    }
 
+    private void placeWalls(Gamefield field) {
+        for (Position position : _config.getWalls()) {
+            Cell cell = field.getCell(position.row(), position.col());
+            cell.putUnit(new Wall());
+        }
+    }
+
+    private void placeIronBlocks(Gamefield field) {
+        for (Position position : _config.getIronBlocks()) {
+            Cell cell = field.getCell(position.row(), position.col());
+            cell.putUnit(new IronBlock());
+        }
+    }
+
+    private void placeLava(Gamefield field) {
+        for (Position position : _config.getLava()) {
+            Cell cell = field.getCell(position.row(), position.col());
+            cell.putUnit(new Lava());
+        }
+    }
+
+    private void placeWater(Gamefield field) {
+        for (Position position : _config.getWater()) {
+            Cell cell = field.getCell(position.row(), position.col());
+            cell.putUnit(new Water());
+        }
+    }
+
+    private void placePlayer(Gamefield field) {
+        Position position = _config.getPlayerPosition();
+        Cell cell = field.getCell(position.row(), position.col());
+        cell.putUnit(new Player());
+    }
+
+    private void placeExit(Gamefield field) {
+        Position position = _config.getExitPosition();
+        Cell cell = field.getCell(position.row(), position.col());
+        cell.putUnit(new Exit());
     }
 }
