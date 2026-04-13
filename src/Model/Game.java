9638d2cd5242ebfaa9ee17220b8bc5bd51c4c8c9
ная <nastya034rus@yandex.ru>
2026-04-13 16:53:48 +0300

    Созданы поля для класса Game

--- src/Model/Game.java
+++ src/Model/Game.java
@@ -1,4 +1,12 @@
 package Model;
 
+import Model.gamefield.Gamefield;
+import Model.services.Maze;
+
 public class Game {
+
+    private boolean _isOver;
+    private Gamefield _field;
+    private Maze _maze;
+    
 }
