b85e1586838e1e5b67909fb9813ae91263e83e28
ная <nastya034rus@yandex.ru>
2026-04-14 00:16:46 +0300

    Добавлены поля и конструктор класса LevelConfig

--- src/Model/services/LevelConfig.java
+++ src/Model/services/LevelConfig.java
@@ -1,4 +1,15 @@
 package Model.services;
 
 public class LevelConfig {
+
+    private final int _height;
+    private final int _width;
+    private final char [][] _layout;
+
+    LevelConfig(int height, int width, char[][] layout) {
+        _height = height;
+        _width = width;
+        _layout = layout;
+    }
+    
 }
