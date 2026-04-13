f553ca887114a0ef326263a6566191a70b0d3e95
ная <nastya034rus@yandex.ru>
2026-04-13 22:33:20 +0300

    Реализация Exit: создано наследование от Unit и реализован метод canBelongTo

--- src/Model/units/Exit.java
+++ src/Model/units/Exit.java
@@ -1,4 +1,11 @@
 package Model.units;
 
-public class Exit {
+import Model.gamefield.Cell;
+
+public class Exit extends Unit {
+
+    @Override
+    protected boolean canBelongTo(Cell cell) {
+        return cell != null && cell.getUnit(Exit.class) == null;
+    }
 }
