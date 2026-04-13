f8638372df3f220226a00feda2a5bfdda6e0de3a
ная <nastya034rus@yandex.ru>
2026-04-14 00:37:07 +0300

    Изменение полей и конструктора класса LevelConfig вследствие появления класса Position и добавление новых полей

--- src/Model/services/LevelConfig.java
+++ src/Model/services/LevelConfig.java
@@ -1,15 +1,28 @@
 package Model.services;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class LevelConfig {
 
     private final int _height;
     private final int _width;
-    private final char [][] _layout;
 
-    LevelConfig(int height, int width, char[][] layout) {
+    private Position _playerPosition;
+    private Position _exitPosition;
+
+    private final List<Position> _walls = new ArrayList<>();
+    private final List<Position> _ironBlocks = new ArrayList<>();
+    private final List<Position> _lava = new ArrayList<>();
+    private final List<Position> _water = new ArrayList<>();
+
+    public LevelConfig(int height, int width) {
+        if (height <= 0 || width <= 0) {
+            throw new IllegalArgumentException("Размеры поля должны быть положительными");
+        }
+
         _height = height;
         _width = width;
-        _layout = layout;
     }
     
 }
