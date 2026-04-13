4fb97a86421179aa14300e08cccc76c49250b86b
ная <nastya034rus@yandex.ru>
2026-04-14 02:03:39 +0300

    Добавлен метод buildField в Maze для построения поля, загулшка под будущий метод equipCells и проверка в конструктор Maze

--- src/Model/services/Maze.java
+++ src/Model/services/Maze.java
@@ -1,10 +1,24 @@
 package Model.services;
 
+import Model.gamefield.Gamefield;
+
 public class Maze {
 
     private final LevelConfig _config;
 
     public Maze(LevelConfig config){
+        if(config == null) {
+            throw new NullPointerException("Невозможно создать игру с null-конфигурацией");
+        }
         _config = config;
+    }
+    public Gamefield buildField() {
+        Gamefield field = new Gamefield(_config.getHeight(), _config.getWidth());
+        equipCells(field);
+        return field;
+    }
+
+    private void equipCells(Gamefield field) {
+
     }
 }
