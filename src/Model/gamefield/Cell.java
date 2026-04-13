74ff16c2131940b8c5a19b2d5c5bbc622420f793
ная <nastya034rus@yandex.ru>
2026-04-13 16:13:15 +0300

    Созданы поля и конструктор класса Cell

--- src/Model/gamefield/Cell.java
+++ src/Model/gamefield/Cell.java
@@ -1,4 +1,23 @@
 package Model.gamefield;
 
+import Model.units.Unit;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
 public class Cell {
+
+    private Map<Direction, Cell> _neighbours = new HashMap<>();
+    private ArrayList<Unit> _units = new ArrayList<>();
+    private Gamefield _field;
+
+    public Cell(Gamefield field){
+
+        if(field == null) {
+            throw new NullPointerException("Игровое поле не может быть null");
+        }
+        
+        _field = field;
+    }
 }
