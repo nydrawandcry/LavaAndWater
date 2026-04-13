a2cb396efd2caae698e26bacbe1e2b5c98e3746f
ная <nastya034rus@yandex.ru>
2026-04-13 16:45:47 +0300

    Реализованы все необходимые методы для работы с Unit(геттеры и сеттеры)

--- src/Model/units/Unit.java
+++ src/Model/units/Unit.java
@@ -7,4 +7,36 @@ public abstract class Unit {
     private Cell _owner;
     private boolean _isActive;
     private boolean _isDestroyed;
+
+    public boolean isActive(){
+        return _isActive;
+    }
+
+    public boolean isDestroyed(){
+        return _isDestroyed;
+    }
+
+    public Cell owner(){
+        return _owner;
+    }
+
+    public void setOwner(Cell cell){
+        if(cell == null) {
+            throw new NullPointerException("Ячейка не должна быть null");
+        }
+
+        _owner = cell;
+    }
+
+    public void removeOwner(){
+        _owner = null;
+    }
+
+    public void activate(){
+        _isActive = true;
+    }
+
+    public void deactivate(){
+        _isActive = false;
+    }
 }
