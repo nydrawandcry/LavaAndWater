7fa67f552539713c993a60804a7f70143c55b717
ная <nastya034rus@yandex.ru>
2026-04-14 02:37:53 +0300

    Удалены старые и добавлены новые поля и геттеры в классе Game

--- src/Model/Game.java
+++ src/Model/Game.java
@@ -1,19 +1,34 @@
 package Model;
 
 import Model.gamefield.Gamefield;
+import Model.services.CollisionDetector;
 import Model.services.Maze;
+import Model.units.Player;
 
 public class Game {
 
     private boolean _isOver;
+    private boolean _isWon;
+
     private Gamefield _field;
-    private Maze _maze;
+    private Player _player;
+    private CollisionDetector _collisionDetector = new CollisionDetector();
+
+    
 
     public Gamefield getField() {
         return _field;
     }
 
-    public Maze getMaze() {
-        return _maze;
+    public Player getPlayer() {
+        return _player;
+    }
+
+    public boolean isOver() {
+        return _isOver;
+    }
+
+    public boolean isWon() {
+        return _isWon;
     }
 }
