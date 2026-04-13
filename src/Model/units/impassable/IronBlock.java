1cb077a0809b43371899eada60ca5bc45604a3dd
ная <nastya034rus@yandex.ru>
2026-04-13 22:03:20 +0300

    Реализовано движение IronBlock игроком

--- src/Model/units/impassable/IronBlock.java
+++ src/Model/units/impassable/IronBlock.java
@@ -1,6 +1,7 @@
 package Model.units.impassable;
 
 import Model.gamefield.Cell;
+import Model.gamefield.Direction;
 import Model.units.Unit;
 
 public class IronBlock extends Unit implements Impassable{
@@ -8,5 +9,19 @@ public class IronBlock extends Unit implements Impassable{
     @Override
     protected boolean canBelongTo(Cell cell) {
         return cell != null && cell.getUnit(Impassable.class) == null;
+    }
+
+    public boolean moveByPlayer(Direction dir) {
+        Cell current = owner();
+        Cell target = owner().getNeighbours().get(dir);
+
+        if(target == null || target.getUnit(Impassable.class) != null) {
+            return false;
+        }
+
+        current.extractUnit(this);
+        target.putUnit(this);
+
+        return true;
     }
 }
