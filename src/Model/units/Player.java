86c175af574bd9d8fe4eea0c5e2c81d1180db0cc
ная <nastya034rus@yandex.ru>
2026-04-13 22:17:13 +0300

    Реализована механика движения Player по полю

--- src/Model/units/Player.java
+++ src/Model/units/Player.java
@@ -1,7 +1,9 @@
 package Model.units;
 
 import Model.gamefield.Cell;
+import Model.gamefield.Direction;
 import Model.units.impassable.Impassable;
+import Model.units.impassable.IronBlock;
 
 public class Player extends Unit {
 
@@ -19,5 +21,29 @@ public class Player extends Unit {
     @Override
     protected boolean canBelongTo(Cell cell) {
         return cell != null && cell.getUnit(Impassable.class) == null;
+    }
+
+    public boolean moveTo(Direction dir) {
+        Cell destination = owner().getNeighbour(dir);
+
+        if(destination == null) {
+            return false;
+        }
+
+        Unit blocking = destination.getUnit(Impassable.class);
+
+        if(blocking instanceof IronBlock) { //мне не нравится тут эта проверка на конкретный класс...
+            if(!((IronBlock) blocking).moveByPlayer(dir)){
+                return false;
+            }
+        } 
+        else if(blocking != null){
+            return false;
+        }
+
+        owner().extractUnit(this);
+        destination.putUnit(this);
+
+        return true;
     }
 }
