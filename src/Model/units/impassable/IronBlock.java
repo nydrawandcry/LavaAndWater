3b10489940e268562c0406b8d398c5ec6bde22f1
ная <nastya034rus@yandex.ru>
2026-04-13 21:39:36 +0300

    Добавление наследования от Unit и реализация метода canBelongTo в классе IronBlock

--- src/Model/units/impassable/IronBlock.java
+++ src/Model/units/impassable/IronBlock.java
@@ -1,4 +1,12 @@
 package Model.units.impassable;
 
-public class IronBlock implements Impassable{
+import Model.gamefield.Cell;
+import Model.units.Unit;
+
+public class IronBlock extends Unit implements Impassable{
+
+    @Override
+    protected boolean canBelongTo(Cell cell) {
+        return cell != null && cell.getUnit(Impassable.class) == null;
+    }
 }
