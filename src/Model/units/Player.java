08ef7aea25b169b8e2f14f5c0f76a20e209c3ca6
ная <nastya034rus@yandex.ru>
2026-04-13 22:07:55 +0300

    Реализовано наследование Player от Unit, реализован метод canBelongTo и добавлен метод kill

--- src/Model/units/Player.java
+++ src/Model/units/Player.java
@@ -1,11 +1,23 @@
 package Model.units;
 
-public class Player {
+import Model.gamefield.Cell;
+import Model.units.impassable.Impassable;
+
+public class Player extends Unit {
 
     private boolean _isAlive;
 
     public boolean isAlive() {
         return _isAlive;
     }
-    
+
+    public void kill() {
+        _isAlive = false;
+        deactivate();
+    }
+
+    @Override
+    protected boolean canBelongTo(Cell cell) {
+        return cell != null && cell.getUnit(Impassable.class) == null;
+    }
 }
