4e4bddd0508a604e56794a8ef3057f1a550bc3ba
ная <nastya034rus@yandex.ru>
2026-04-13 18:47:52 +0300

    Добавлена проверка, может ли жидкость распространиться на заданную клетку, в класс Liquid

--- src/Model/units/liquids/Liquid.java
+++ src/Model/units/liquids/Liquid.java
@@ -1,8 +1,24 @@
 package Model.units.liquids;
 
+import Model.gamefield.Cell;
 import Model.units.Unit;
+import Model.units.impassable.Impassable;
 
 public abstract class Liquid extends Unit {
+
+    private boolean canEnter(Cell cell) {
+        if(cell == null) {
+            return false;
+        }
+        if(cell.getUnit(Impassable.class) != null) { //переработать метод надо, чтобы с интерфейсами тоже работало
+            return false;
+        }
+        if(cell.getUnit(this.getClass()) != null) {
+            return false;
+        }
+
+        return true;
+    }
 
     protected abstract Liquid createInstance();
 }
