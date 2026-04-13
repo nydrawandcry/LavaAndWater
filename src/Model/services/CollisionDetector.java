1bcc2303612d578644e76068eb49bfd667f254a8
ная <nastya034rus@yandex.ru>
2026-04-14 02:13:18 +0300

    Реализован метод по созданию стен на границе столкновения лавы и воды в CollisionDetector

--- src/Model/services/CollisionDetector.java
+++ src/Model/services/CollisionDetector.java
@@ -1,4 +1,16 @@
 package Model.services;
 
+import Model.gamefield.Cell;
+import Model.units.impassable.Wall;
+import Model.units.liquids.Lava;
+import Model.units.liquids.Water;
+
 public class CollisionDetector {
+
+    private void createWallsInCell(Cell cell) {
+        cell.extractUnit(cell.getUnit(Lava.class));
+        cell.extractUnit(cell.getUnit(Water.class));
+
+        cell.putUnit(new Wall());
+    }
 }
