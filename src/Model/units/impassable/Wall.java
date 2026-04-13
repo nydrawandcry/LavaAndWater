360ac2ceb714e83cae95afb96f796f174d8ceb5b
ная <nastya034rus@yandex.ru>
2026-04-13 21:29:49 +0300

    Реализация класса Wall: добавление наследования от Unit и реализация метода canBelongTo

--- src/Model/units/impassable/Wall.java
+++ src/Model/units/impassable/Wall.java
@@ -1,4 +1,12 @@
 package Model.units.impassable;
 
-public class Wall implements Impassable {
+import Model.gamefield.Cell;
+import Model.units.Unit;
+
+public class Wall extends Unit implements Impassable {
+
+    @Override
+    protected boolean canBelongTo(Cell cell) {
+        return cell != null && cell.getUnit(Impassable.class) == null; //может стоять на клетках, где еще нет барьеров и если клетка существует
+    }
 }
