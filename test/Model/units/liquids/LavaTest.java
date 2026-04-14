9266a488a08c16ae29fb3836195d165142618e99
ная <nastya034rus@yandex.ru>
2026-04-14 05:31:57 +0300

    Реализация наследование тестовых классов в LavaTest и WaterTest

--- test/Model/units/liquids/LavaTest.java
+++ test/Model/units/liquids/LavaTest.java
@@ -1,4 +1,9 @@
 package Model.units.liquids;
 
-public class LavaTest {
+public class LavaTest extends AbstractLiquidTest<Lava> {
+
+    @Override
+    protected Lava createUnit() {
+        return new Lava();
+    }
 }
--- test/Model/units/liquids/WaterTest.java
+++ test/Model/units/liquids/WaterTest.java
@@ -1,4 +1,8 @@
 package Model.units.liquids;
 
-public class WaterTest {
+public class WaterTest extends AbstractLiquidTest<Water> {
+    @Override
+    protected Water createUnit() {
+        return new Water();
+    }
 }
