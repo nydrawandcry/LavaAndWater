72ac54f0fbcac3d584cf5ad59c5806e55a390433
ная <nastya034rus@yandex.ru>
2026-04-14 03:43:46 +0300

    Подключены необходимые библиотеки для тестирования класса Direction, созданы пустые тесты для дальнейшей реализации

--- test/Model/gamefield/DirectionTest.java
+++ test/Model/gamefield/DirectionTest.java
@@ -1,4 +1,48 @@
 package Model.gamefield;
 
+import Model.gamefield.Direction;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
 public class DirectionTest {
+
+    @Test
+    void clockwise_rotatesDirectionCorrectly() {
+        
+    }
+
+    @Test
+    void anticlockwise_rotatesDirectionCorrectly() {
+
+    }
+
+    @Test
+    void opposite_returnsCorrectDirection() {
+
+    }
+
+    @Test
+    void isOpposite_returnsTrueForOppositeDirections() {
+
+    }
+
+    @Test
+    void isOpposite_returnsFalseForNonOppositeDirections() {
+
+    }
+
+    @Test
+    void onRight_returnsRightDirection() {
+
+    }
+
+    @Test
+    void onLeft_returnsLeftDirection() {
+
+    }
+
+    @Test
+    void toString_returnsCorrectLetter() {
+
+    }
 }
