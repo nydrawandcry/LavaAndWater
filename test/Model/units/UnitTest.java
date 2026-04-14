23b985e45fea1660a906dda704628acbd9e3c8f2
ная <nastya034rus@yandex.ru>
2026-04-14 04:52:19 +0300

    Добавлены абстрактные тесты для класса Unit

--- test/Model/units/UnitTest.java
+++ test/Model/units/UnitTest.java
@@ -1,4 +1,21 @@
 package Model.units;
 
-public class UnitTest {
+import org.junit.jupiter.api.Test;
+
+public abstract class UnitTest {
+
+    @Test
+    public abstract void setOwnerTest();
+
+    @Test
+    public abstract void removeOwnerTest();
+
+    @Test
+    public abstract void activateTest();
+
+    @Test
+    public abstract void deactivateTest();
+
+    @Test
+    public abstract void destroy_removesUnitFromCell();
 }
