66ed12736a6fe07d88174fd5f60f9869db895f4b
ная <nastya034rus@yandex.ru>
2026-04-14 03:41:15 +0300

    Расширен функционал Direction, добавлены методы для противоположных направлений, определения новых направлений и представления в строке

--- src/Model/gamefield/Direction.java
+++ src/Model/gamefield/Direction.java
@@ -12,5 +12,58 @@ public enum Direction {
     Direction(int hours){
         _hours = hours % 12;
     }
-    
+
+    public Direction clockwise() {
+        return fromHours(_hours + 3);
+    }
+
+    public Direction anticlockwise() {
+        return fromHours(_hours - 3);
+    }
+
+    public Direction opposite() {
+        return fromHours(_hours + 6);
+    }
+
+    public Direction onRight() {
+        return clockwise();
+    }
+
+    public Direction onLeft() {
+        return anticlockwise();
+    }
+
+    private static Direction fromHours(int hours){
+        int normalized = (hours % 12 + 12) % 12;
+
+        for(Direction dir : values()){
+            if(dir._hours == normalized){
+                return dir;
+            }
+        }
+        throw new IllegalArgumentException("Неверное направление");
+    }
+
+    // ---------------------------------------
+
+    public boolean isOpposite(Direction other) {
+        return this.opposite().equals(other);
+    }
+
+    @Override
+    public String toString() {
+        String msg = "";
+
+        if(_hours == 0) {
+            msg = "N";
+        } else if(_hours == 3) {
+            msg = "E";
+        } else if (_hours == 6) {
+            msg = "S";
+        } else if(_hours == 9) {
+            msg = "W";
+        }
+
+        return msg;
+    }
 }
