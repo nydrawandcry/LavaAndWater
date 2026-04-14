b13d49c242273426039ae3aac7d9e6933e6ee40a
ная <nastya034rus@yandex.ru>
2026-04-14 05:08:42 +0300

    Реализация тестов для Wall

--- test/Model/units/impassable/WallTest.java
+++ test/Model/units/impassable/WallTest.java
@@ -2,11 +2,27 @@ package Model.units.impassable;
 
 import Model.units.AbstractUnitTest;
 import Model.units.Unit;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 public class WallTest extends AbstractUnitTest {
 
     @Override
     protected Unit createUnit() {
-        return null;
+        return new Wall();
+    }
+
+    @Test
+    void canBelongTo_emptyCell_returnsTrue() {
+        assertTrue(unit.canBelongTo(cell));
+    }
+
+    @Test
+    void canBelongTo_cellWithImpassable_returnsFalse() {
+        cell.putUnit(new Wall());
+
+        assertFalse(unit.canBelongTo(cell));
     }
 }
