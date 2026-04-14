a87b5c8130302ec6551ff392c5240b727e4a8534
ная <nastya034rus@yandex.ru>
2026-04-14 05:27:04 +0300

    Исправление метода в Exit после проверки на модульном тестировании

--- src/Model/units/Exit.java
+++ src/Model/units/Exit.java
@@ -1,11 +1,12 @@
 package Model.units;
 
 import Model.gamefield.Cell;
+import Model.units.impassable.Impassable;
 
 public class Exit extends Unit {
 
     @Override
     public boolean canBelongTo(Cell cell) {
-        return cell != null && cell.getUnit(Exit.class) == null;
+        return cell != null && cell.getUnit(Exit.class) == null && cell.getUnit(Impassable.class) == null;
     }
 }
