effa913470e2cdfc40c3b4cb78d691b586fca9dc
ная <nastya034rus@yandex.ru>
2026-04-14 05:24:36 +0300

    Реализация тестов для Exit

--- test/Model/units/ExitTest.java
+++ test/Model/units/ExitTest.java
@@ -1,9 +1,38 @@
 package Model.units;
 
+import Model.units.impassable.Wall;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.Assert.*;
+
 public class ExitTest extends AbstractUnitTest<Exit> {
 
     @Override
     protected Exit createUnit() {
         return new Exit();
+    }
+
+    @Test
+    void canBelongTo_emptyCell_returnsTrue() {
+        assertTrue(unit.canBelongTo(cell));
+    }
+
+    @Test
+    void canBelongTo_cellWithWall_returnsFalse() {
+        cell.putUnit(new Wall());
+
+        assertFalse(unit.canBelongTo(cell));
+    }
+
+    @Test
+    void canShareCellWithPlayer() {
+        cell.putUnit(unit);
+        Player player = new Player();
+
+        boolean result = cell.putUnit(player);
+
+        assertTrue(result);
+        assertNotNull(cell.getUnit(Exit.class));
+        assertNotNull(cell.getUnit(Player.class));
     }
 }
