d64b96beeecd4eeccf6fa696ee3f6d9a37147fec
ная <nastya034rus@yandex.ru>
2026-05-04 21:59:01 +0300

    первичная реализация GameFrame (без событий, а также надо доработать класс Game)

--- src/View/gamefieldView/GameFrame.java
+++ src/View/gamefieldView/GameFrame.java
@@ -1,4 +1,36 @@
 package View.gamefieldView;
 
-public class GameFrame {
+import Model.Game;
+
+import javax.swing.*;
+
+public class GameFrame extends JFrame {
+
+    private Game _game;
+    private GamefieldView _fieldView;
+
+    public GameFrame() {
+        super("Lava & Water");
+
+        startNewGame();
+
+        pack();
+
+        setResizable(true);
+        setLocationRelativeTo(null);
+
+
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+
+    private void startNewGame() {
+        if(_game != null) {
+            return; //пока не знаю что сюда надо добавить
+        }
+
+        //тут создание игры и подписка GameFrame на события Game (и еще манипуляции с gamefieldView)
+
+        revalidate();
+        repaint();
+    }
 }
