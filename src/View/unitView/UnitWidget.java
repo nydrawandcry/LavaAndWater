2ebf777992b74ef070a99be1da1fde77aa4b2761
ная <nastya034rus@yandex.ru>
2026-05-10 00:56:41 +0300

    реализован класс виджета для юнита (абстрактный UnitWidget)

--- src/View/unitView/UnitWidget.java
+++ src/View/unitView/UnitWidget.java
@@ -1,4 +1,36 @@
 package View.unitView;
 
-public abstract class UnitWidget {
+import Model.units.Unit;
+
+import javax.swing.*;
+import java.awt.*;
+
+public abstract class UnitWidget extends JComponent {
+
+    protected Unit _unit;
+
+    protected UnitWidget(Unit unit) {
+        _unit = unit;
+        //подписка unit на событие
+
+        setOpaque(true);
+    }
+
+    public Unit getUnit() {
+        return _unit;
+    }
+
+    protected abstract Color getActiveColor();
+    protected abstract Color getInactiveColor();
+
+    protected abstract void refresh();
+    protected abstract void changeColor(Color c);
+
+    protected void changeColorByActivity() {
+        if(_unit.isActive()){
+            changeColor(getActiveColor());
+        } else {
+            changeColor(getInactiveColor());
+        }
+    }
 }
