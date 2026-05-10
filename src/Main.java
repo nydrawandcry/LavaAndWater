64f6e0de0600cf2ec016635a882de3704f4ea333
ная <nastya034rus@yandex.ru>
2026-05-10 14:32:19 +0300

    создана функция main для запуска игры

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,17 @@
+import View.gamefieldView.GameFrame;
+
+import javax.swing.*;
+
 public class Main {
+
+    public static void main(String[] args) {
+
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                GameFrame mainW = new GameFrame();
+                mainW.setVisible(true);
+            }
+        });
+    }
 }
