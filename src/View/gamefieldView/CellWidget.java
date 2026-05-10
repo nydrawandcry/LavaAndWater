eb19d23438d264bf870c72d70b0940c908c7dbc5
ная <nastya034rus@yandex.ru>
2026-05-10 11:49:18 +0300

    создан первичный конструктор CellWidget и инициализация виджета юнитами

--- src/View/gamefieldView/CellWidget.java
+++ src/View/gamefieldView/CellWidget.java
@@ -1,5 +1,7 @@
 package View.gamefieldView;
 
+import Model.gamefield.Cell;
+
 import javax.swing.*;
 import java.awt.*;
 
@@ -7,6 +9,61 @@ public class CellWidget extends JPanel {
 
     public static int CELL_SIZE = 50;
 
+    private final Cell _cell;
+
+    private JLayeredPane _layeredPane;
+
+    private JPanel _playerLayer;
+    private JPanel _wallLayer;
+    private JPanel _exitLayer;
+    private JPanel _ironBlockLayer;
+
+    private JPanel _liquidLayer; //todo затравка на жидкости (попозже сделаю их)
+
+    public CellWidget(Cell cell){
+        _cell = cell;
+        //подписка на события
+
+        setLayout(new BorderLayout());
+        setPreferredSize(new Dimension(CELL_SIZE, CELL_SIZE));
+        setOpaque(true);
+
+        //инициализация юнитами
+        initializeUnitLayers();
+    }
+
+    private void initializeUnitLayers() {
+        //первоначальная инициализация pane (порождение и установка размера)
+        _layeredPane = new JLayeredPane();
+        _layeredPane.setPreferredSize(new Dimension(CELL_SIZE, CELL_SIZE));
+
+        _playerLayer = new JPanel();
+        _playerLayer.setLayout(null); //по идее игрок у нас просто в клетке стоит?(я пока не разобралась с лэйаутами, попозже разберусь и настрою нормально)
+        _playerLayer.setOpaque(false); //че это я тоже не ебу
+        _playerLayer.setBounds(2,2, CELL_SIZE, CELL_SIZE);
+
+        _wallLayer = new JPanel();
+        _wallLayer.setLayout(null);
+        _wallLayer.setOpaque(false);
+        _wallLayer.setBounds(2,2, CELL_SIZE, CELL_SIZE);
+
+        _ironBlockLayer = new JPanel();
+        _ironBlockLayer.setLayout(null);
+        _ironBlockLayer.setOpaque(false);
+        _ironBlockLayer.setBounds(2,2, CELL_SIZE, CELL_SIZE);
+
+        _exitLayer = new JPanel();
+        _exitLayer.setLayout(null);
+        _exitLayer.setOpaque(false);
+        _exitLayer.setBounds(2,2, CELL_SIZE, CELL_SIZE);
+
+        _layeredPane.add(_playerLayer, JLayeredPane.DEFAULT_LAYER);
+        _layeredPane.add(_wallLayer, JLayeredPane.DEFAULT_LAYER);
+        _layeredPane.add(_ironBlockLayer, JLayeredPane.DEFAULT_LAYER);
+        _layeredPane.add(_exitLayer, JLayeredPane.DEFAULT_LAYER); //todo я пересмотрю такую архитектуру, дублирование кода.надо переделать
+
+        add(_layeredPane, BorderLayout.CENTER);
+    }
 
     void changeColor(Color c) {
         setBackground(c);
