fe15389a97c88872348a580c765544230978a834
ная <nastya034rus@yandex.ru>
2026-05-18 22:09:46 +0300

    в класс Exit добавлено поле о количестве очках активации, а также добавлены методы по работе с этим количеством (геттер и снижение оставшегося количества)

--- src/Model/units/Exit.java
+++ src/Model/units/Exit.java
@@ -5,8 +5,18 @@ import Model.units.solid.Solid;
 
 public class Exit extends Unit {
 
+    private int _leftScores = 3;
+
     @Override
     public boolean canBelongTo(Cell cell) {
         return cell != null && cell.getUnit(Exit.class) == null && cell.getUnit(Solid.class) == null;
+    }
+
+    public int getLeftScores() {
+        return _leftScores;
+    }
+
+    public void decreaseLeftScores() {
+        _leftScores--;
     }
 }
